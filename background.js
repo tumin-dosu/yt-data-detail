fc57a29a17f93be34e41dd51b389a182a3d7b295
tumin-dosu <sfgemas06@gmail.com>
2025-07-08 01:31:05 +0900

    各ファイルにバージョンの追記

--- background.js
+++ background.js
@@ -1,4 +1,13 @@
 // background.js
+/**
+ * @file YouTube Absolute Date Display Extension
+ * @version 2.4 (Security Enhanced)
+ * @author tumin-dosu
+ * @date 2025-07-07
+ * @description YouTube動画の相対日付表示の横に投稿年月日を追加するChrome拡張機能
+ * @contact : tumin_sfre@outlook.com
+ * Copyright 2025 tumin-dosu. All rights reserved.
+ */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'openOptions') {
     chrome.runtime.openOptionsPage();
